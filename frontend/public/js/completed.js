50bc893f9df5d1455c572f65e8002c88cad97e3f
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2024-12-05 10:29:00 +0700

    Update completed.js

--- frontend/public/js/completed.js
+++ frontend/public/js/completed.js
@@ -1,4 +1,3 @@
-
 let tasks;
 
 // Script để gọi API từ server
@@ -17,7 +16,6 @@ async function fetchDataFromServer() {
     if (Array.isArray(responseData)) {
       tasks = responseData;
     } else if (typeof responseData === 'object') {
-      // Nếu là đối tượng, tạo một mảng chứa đối tượng đó
       tasks = [responseData];
     } else {
       console.error('Data from server is not an array or object:', responseData);
@@ -51,7 +49,6 @@ async function fetchDataFromServer() {
       const statusDiv = document.createElement('div');
       statusDiv.classList.add('status');
 
-      // Kiểm tra isImportant và isCompleted để thêm chữ important và completed
       if (task.isImportant) {
         const importantSpan = document.createElement('span');
         importantSpan.classList.add('important_task');
@@ -79,15 +76,17 @@ async function fetchDataFromServer() {
       updateButton.textContent = 'Update';
       updateButton.addEventListener('click', () => openModal('modalUpdate' + index));
 
-      // Tạo button Delete
-      const deleteButton = document.createElement('button');
-      deleteButton.classList.add('delete');
-      deleteButton.textContent = 'Delete';
-      deleteButton.addEventListener('click', () => openModal('modalDelete' + index));
+      const commentButton = document.createElement('button');
+      commentButton.classList.add('comment');
+      commentButton.textContent = 'Comment';
+      commentButton.addEventListener('click', () => {
+        localStorage.setItem("currentTask", task.task_id);
+        openCommentForm(task); // Gọi hàm mở form comment khi nhấn button "Comment"
+      });
 
       // Thêm button vào div chứa các button
       actionButtonsDiv.appendChild(updateButton);
-      actionButtonsDiv.appendChild(deleteButton);
+      actionButtonsDiv.appendChild(commentButton);
 
       // Thêm div chứa các button vào taskDiv
       taskDiv.appendChild(actionButtonsDiv);
@@ -98,123 +97,182 @@ async function fetchDataFromServer() {
       // Thêm modal Update
       const modalUpdate = createModal('modalUpdate' + index, 'Update Task', task);
       taskContainer.appendChild(modalUpdate);
-
-      // Thêm modal Delete
-      const modalDelete = createModal('modalDelete' + index, 'Delete Task', task);
-      taskContainer.appendChild(modalDelete);
     });
 
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
+
 function createModal(modalId, modalTitle, task) {
-    const modal = document.createElement('div');
-    modal.classList.add('modal');
-    modal.id = modalId;
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.id = modalId;
 
-    const modalContent = document.createElement('div');
-    modalContent.classList.add('modal-content');
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
 
-    const closeSpan = document.createElement('span');
-    closeSpan.className = 'close';
-    closeSpan.innerHTML = '&times;';
+  const closeSpan = document.createElement('span');
+  closeSpan.className = 'close';
+  closeSpan.innerHTML = '&times;';
+  closeSpan.addEventListener('click', () => closeModal(modalId));
+  modalContent.appendChild(closeSpan);
 
-    closeSpan.addEventListener('click', () =>closeModal(modalId));
-    modalContent.appendChild(closeSpan);
+  const modalTitleDiv = document.createElement('h2');
+  modalTitleDiv.textContent = modalTitle;
+  modalContent.appendChild(modalTitleDiv);
 
-    const modalTitleDiv = document.createElement('h2');
-    modalTitleDiv.textContent = modalTitle;
-    modalContent.appendChild(modalTitleDiv);
+  const modalForm = document.createElement('form');
+  const inputTitle = createInput('Title', 'text', task.title);
+  inputTitle.className = 'newinfo';
+  modalForm.appendChild(inputTitle);
 
-    
-    const modalForm = document.createElement('form');
-    // modalForm.classList.add('form');
+  const inputDueDate = createInput('Due Date', 'date', task.dueDate);
+  inputDueDate.className = 'newinfo';
+  modalForm.appendChild(inputDueDate);
 
-    const inputTitle = createInput('Title', 'text', task.title);
-    inputTitle.className = 'newinfo';
-    modalForm.appendChild(inputTitle);
+  const inputDescription = createInput('Description', 'text', task.description);
+  inputDescription.className = 'newinfo';
+  modalForm.appendChild(inputDescription);
 
-    const inputDueDate = createInput('Due Date', 'date', task.dueDate);
-    inputDueDate.className = 'newinfo';
-    modalForm.appendChild(inputDueDate);
+  const inputIsImportant = createInput('Is Important', 'checkbox', task.isImportant);
+  inputIsImportant.className = 'newinfo';
+  modalForm.appendChild(inputIsImportant);
 
-    const inputDescription = createInput('Description', 'text', task.description);
-    inputDescription.className = 'newinfo';
-    modalForm.appendChild(inputDescription);
+  const inputIsCompleted = createInput('Is Completed', 'checkbox', task.isCompleted);
+  inputIsCompleted.className = 'newinfo';
+  modalForm.appendChild(inputIsCompleted);
 
-    const inputIsImportant = createInput('Is Important', 'checkbox', task.isImportant);
-    inputIsImportant.className = 'newinfo';
-    modalForm.appendChild(inputIsImportant);
+  if (modalTitle === 'Update Task') {
+    const saveButton = document.createElement('button');
+    saveButton.className = 'save-button';
+    saveButton.textContent = 'Save Update';
+    saveButton.addEventListener('click', () => closeModal(modalId));
+    modalForm.appendChild(saveButton);
+  }
 
-    const inputIsCompleted = createInput('Is Completed', 'checkbox', task.isCompleted);
-    inputIsCompleted.className = 'newinfo';
-    modalForm.appendChild(inputIsCompleted);
+  modalContent.appendChild(modalForm);
+  modal.appendChild(modalContent);
 
-    if(modalTitle==='Update Task'){
-        const saveButton = document.createElement('button');
-        saveButton.className = 'save-button';
-        saveButton.textContent = 'Save Update';
-        saveButton.addEventListener('onclick', () => closeModal(modalId));
-        modalForm.appendChild(saveButton);
-    } else{
-        const deleteButton = document.createElement('button');
-        deleteButton.className = 'delete-button';
-        deleteButton.textContent = 'Delete Task';
-        deleteButton.addEventListener('onclick', () => closeModal(modalId));
-        modalForm.appendChild(deleteButton);
-    }
-
-    modalContent.appendChild(modalForm);
-    modal.appendChild(modalContent);
-
-    return modal;
+  return modal;
 }
 
 function createInput(labelText, inputType, value) {
-    const inputDiv = document.createElement('div');
-    const label = document.createElement('label');
-    label.textContent = labelText + ':';
-    inputDiv.appendChild(label);
+  const inputDiv = document.createElement('div');
+  const label = document.createElement('label');
+  label.textContent = labelText + ':';
+  inputDiv.appendChild(label);
 
-    const input = document.createElement('input');
-    input.type = inputType;
-    if(input.type==='checkbox') {
-        if(value===1) {input.value=true;}
-        else {input.value=false;}
-    } else {
-        input.value = value;
-    }
+  const input = document.createElement('input');
+  input.type = inputType;
+  if (input.type === 'checkbox') {
+    input.checked = value;
+  } else {
+    input.value = value;
+  }
 
-    switch (labelText) {
-        case "Title":
-            input.id = "title";
-            break;
-        case "Due Date":
-            input.id = "dueDate";
-            break;
-        case "Description":
-            input.id = "description";
-            break;
-        case "Is Important":
-            input.id = "isImportant";
-            break;
-        // Thêm các trường hợp khác nếu cần
-        default:
-            input.id = 'isCompleted';
-    }
-    
-    inputDiv.appendChild(input);
+  switch (labelText) {
+    case "Title":
+      input.id = "title";
+      break;
+    case "Due Date":
+      input.id = "dueDate";
+      break;
+    case "Description":
+      input.id = "description";
+      break;
+    case "Is Important":
+      input.id = "isImportant";
+      break;
+    default:
+      input.id = 'isCompleted';
+  }
 
-    return inputDiv;
+  inputDiv.appendChild(input);
+
+  return inputDiv;
 }
 
 function openModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.style.display = 'block';
+  const modal = document.getElementById(modalId);
+  modal.style.display = 'block';
 }
 
 function closeModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.style.display = 'none';
+  const modal = document.getElementById(modalId);
+  modal.style.display = 'none';
+}
+
+// Hàm mở form comment
+function openCommentForm(task) {
+  const taskContainer = document.getElementById('taskContainer');
+
+  const commentModal = document.createElement('div');
+  commentModal.classList.add('modal');
+  commentModal.id = 'commentModal' + task.task_id;
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+
+  const closeSpan = document.createElement('span');
+  closeSpan.className = 'close';
+  closeSpan.innerHTML = '&times;';
+  closeSpan.addEventListener('click', () => closeModal('commentModal' + task.task_id));
+  modalContent.appendChild(closeSpan);
+  
+  const modalTitleDiv = document.createElement('h2');
+  modalTitleDiv.textContent = 'Add Comment';
+  modalContent.appendChild(modalTitleDiv);
+
+  const commentForm = document.createElement('form');
+
+  const commentTextArea = document.createElement('textarea');
+  commentTextArea.placeholder = 'Write your comment here...';
+  commentForm.appendChild(commentTextArea);
+
+  const submitButton = document.createElement('button');
+  submitButton.textContent = 'Submit Comment';
+  submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    saveComment(task.task_id, commentTextArea.value);
+  });
+
+  commentForm.appendChild(submitButton);
+  modalContent.appendChild(commentForm);
+
+  commentModal.appendChild(modalContent);
+  taskContainer.appendChild(commentModal);
+
+  openModal('commentModal' + task.task_id);
+}
+
+// Lưu comment vào server
+async function saveComment(taskId, comment) {
+  try {
+    const userId = localStorage.getItem('userID');
+
+    const response = await fetch('http://localhost:3001/api/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        taskId: taskId,
+        userId: userId,
+        comment: comment,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    if (responseData.success) {
+      alert('Comment submitted successfully!');
+      closeModal('commentModal' + taskId);
+    } else {
+      alert('Failed to submit comment.');
+    }
+  } catch (error) {
+    console.error('Error saving comment:', error);
+    alert('There was an error saving the comment.');
+  }
 }
