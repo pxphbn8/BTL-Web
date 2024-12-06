33684216c14f0fa53742981c5872da9807d3a556
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2024-12-06 21:30:04 +0700

    Update web.js

--- Front-end/web.js
+++ Front-end/web.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
