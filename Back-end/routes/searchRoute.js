51152f188b6e02e6751eb11056162e6c42da136d
pxphbn8 <vuducluong12a@gmail.com>
2024-12-12 16:49:40 +0700

    delete dupli file

--- Back-end/routes/searchRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { searchTask } from '../controllers/searchController.js';
-
-const searchRoutes = express.Router();
-
-// Định nghĩa các route và các hàm xử lý tương ứng
-    searchRoutes.get('/searchTask/:userID/:input', searchTask);
-
-export default searchRoutes;
