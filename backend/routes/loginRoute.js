d48d525629a00b7f10abaee89d9b081ded775dd4
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2025-01-01 17:26:04 +0700

    Delete backend directory

--- backend/routes/loginRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { checkLogin } from '../controllers/loginController.js';
-
-const loginRoutes = express.Router();
-
-// Check login
-loginRoutes.post('/check', checkLogin);
-
-export default loginRoutes;
